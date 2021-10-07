c7b7e0425af21a20ba5ad24f579451dbbdd9b13c
woodwardtw <twwoodward@me.com>
2021-10-07 12:43:43 -0400

    single documentation progress

---
+++ loop-templates/content-single-article.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Single article partial template
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<header class="entry-header">
+
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+
+			<?php //understrap_posted_on(); ?>
+
+		</div><!-- .entry-meta -->
+
+	</header><!-- .entry-header -->
+
+	<?php //echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+
+	<div class="entry-content">
+
+		<?php
+		//the_content();
+		echo dlinq_app_overview();
+		echo dlinq_section_repeater();
+		understrap_link_pages();
+		?>
+
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+
+		<?php understrap_entry_footer(); ?>
+
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->

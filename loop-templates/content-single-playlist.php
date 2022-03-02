d47bc1f272c3ffe75b2c8f4429f7174f58b8e6be
woodwardtw <twwoodward@me.com>
2022-03-02 13:16:03 -0500

    better playlist layout
    issue w inc/editor-color-palette.json not auto creating correctly

--- loop-templates/content-single-playlist.php
+++ loop-templates/content-single-playlist.php
@@ -25,7 +25,7 @@ defined( 'ABSPATH' ) || exit;
 
 	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
 
-	<div class="entry-content">
+	<div class="entry-content playlist-content">
 
 		<?php
 		the_content();
